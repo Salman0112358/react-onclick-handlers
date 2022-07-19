d13f8e4a7b9eb7430ae970bd256f3701e414f1c3
Salman H <Salman0112358@outlook.com>
2022-07-19 14:59:26 +0100

    changing button press to  single line anonymous arrow function

--- src/components/TestButton.tsx
+++ src/components/TestButton.tsx
@@ -1,5 +1,48 @@
+/*
 function TestButton(): JSX.Element {
-  return <button>Make me do something when I'm clicked...</button>;
+  function handleClick(){
+    console.log("Greetings from the test button!")
+  }
+  return <button onClick={handleClick}>Make me do something when I'm clicked...</button>;
+}
+---------------------------------------------------------------------EXAMPLE A function declaration-----------------------------------------------------------------------------------------------
+
+function TestButton(): JSX.Element {
+  const handleClick = () => {
+    console.log("Hello from the test button again!!");
+  };
+  return (
+    <button onClick={handleClick}>
+      Make me do something when I'm clicked..
+    </button>
+  );
 }
 
+//---------------------------------------------------------------------EXAMPLE B full body arrow function-----------------------------------------------------------------------------------------------
+
+
+function TestButton() : JSX.Element {
+  const handleClick = () => console.log("single line arrow function")
+  return (
+    <button onClick={handleClick}>Hell from the test button for example C </button>
+  )
+}
+
+
+
+
+//---------------------------------------------------------------------EXAMPLE C Single line arrow function-----------------------------------------------------------------------------------------------*/
+
+function TestButton() : JSX.Element {
+  return (
+    <button onClick={()=> console.log("anonymous function LETS GOOO!!!!")}>
+      This is the test button again! but this time it is anonymous
+    </button>
+  )
+}
+
+
 export default TestButton;
+
+//---------------------------------------------------------------------EXAMPLE F Single line anonymous arrow function-----------------------------------------------------------------------------------------------
+
